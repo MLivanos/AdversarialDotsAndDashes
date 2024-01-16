8283952d031850a4667403775acad7b8d354c6b4
MLivanos <prodigiousmike@gmail.com>
2024-01-15 23:20:00 -0800

    ✨ ADD: Adds ability to create dashes

--- AdversarialDotsAndDashes/Assets/Scripts/DotsAndDashesGame.cs
+++ AdversarialDotsAndDashes/Assets/Scripts/DotsAndDashesGame.cs
@@ -6,6 +6,7 @@ public class DotsAndDashesGame : MonoBehaviour
 {
     [SerializeField] private Vector2Int shape;
     [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private GameObject linePrefab;
     [SerializeField] private float spaceBetweenDots;
     int[,] gameMatrix;
     GameObject[,] gameMatrixObjects;
@@ -26,8 +27,29 @@ public class DotsAndDashesGame : MonoBehaviour
             {
                 Vector3 position = new Vector3(i*spaceBetweenDots - xOffset, j*spaceBetweenDots - yOffset, 0);
                 GameObject newDot = Instantiate(dotPrefab, position, Quaternion.identity);
+                AddNeighborDashes(position);
             }
         }
         Camera.main.orthographicSize = Mathf.Max(shape.x, shape.x);
+    }
+
+    private void AddNeighborDashes(Vector3 position)
+    {
+        if (position.x > 0)
+        {
+            Instantiate(linePrefab, position + Vector3.left, Quaternion.Euler(0, 0, 90));
+        }
+        if (position.x < shape.x - 1)
+        {
+            Instantiate(linePrefab, position + Vector3.right, Quaternion.Euler(0, 0, 90));
+        }
+        if (position.y > 0)
+        {
+            Instantiate(linePrefab, position + Vector3.down, Quaternion.identity);
+        }
+        if (position.y < shape.y - 1)
+        {
+            Instantiate(linePrefab, position + Vector3.up, Quaternion.identity);
+        }
     }
 }
